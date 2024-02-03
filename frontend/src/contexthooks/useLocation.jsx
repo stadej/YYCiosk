9b57cff205dca12d2cceaeebf5b7918f77c052ac
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-02-03 13:29:26 -0700

    Add UseLocation

---
+++ frontend/src/contexthooks/useLocation.jsx
@@ -0,0 +1,67 @@
+import {createContext, useContext, useEffect, useState} from "react";
+
+const LocationContext = createContext()
+// eslint-disable-next-line react/prop-types
+export const LocationProvider =({children})=>{
+    const [location, setLocation] = useState()
+
+    let options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
+    const error = (err)=>{
+        console.log(err)
+    }
+    const success =(pos)=>{
+        // console.log(pos)
+        setLocation([pos.coords.latitude,pos.coords.longitude])
+    }
+    const getLocation=()=>{
+        return location;
+    }
+    useEffect(() => {
+        if(navigator.geolocation){
+            navigator.permissions.query(
+                {name:"geolocation"}
+            ).then(
+                (result)=>{
+                    switch(result.state){
+                        case "granted":
+                            navigator.geolocation.getCurrentPosition(
+                                success,
+                                error,
+                                options
+                                )
+                            break
+                        case "prompt":
+                            navigator.geolocation.getCurrentPosition(
+                                success,
+                                error,
+                                options
+                            )
+                            break
+                        case "denied":
+                            break
+                    }
+                }
+            )
+        }else{
+            console.log("Geolocation is not supported by this browser")
+        }
+    }, []);
+
+    return(
+        <LocationContext.Provider value={
+            {
+                getLocation
+            }
+        }>
+            {children}
+        </LocationContext.Provider>
+    )
+}
+
+export const useLocation=()=>{
+    return(useContext(LocationContext))
+}

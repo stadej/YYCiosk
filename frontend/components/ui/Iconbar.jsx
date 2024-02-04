4ac24e2ae0565ca8a4192f7c735a529c1ce443d8
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-02-04 10:17:18 -0700

    Update Iconbar.jsx

--- frontend/components/ui/Iconbar.jsx
+++ frontend/components/ui/Iconbar.jsx
@@ -17,20 +17,19 @@ function Iconbar() {
     <div className='dashboard--icons sticky justify-self-end'>
       {/* div that has background color red, and holds icon divs */}
       <div style={{display: 'flex'}}>
-        {Object.values(layerProvider.getLayers()).map(layer=>{
-          console.log(layer, language)
-          return(
-              <DashboardIcon key={layer.tagName} handleClick={layerProvider.handleClick} imageIcon={layer.imageUrl} iconLabel={language[layer.tagName]}/>
-          )
-        })}
-        {/*<DashboardIcon imageIcon={"../../src/assets/food_icon.png"} iconLabel={"Food"}/>*/}
-        {/*<DashboardIcon imageIcon={"../../src/assets/bathroom_icon.png"} iconLabel={"Bathroom"}/>*/}
-        {/*<DashboardIcon imageIcon={"../../src/assets/trash_icon.png"} iconLabel={"Trash"}/>*/}
-        {/*<DashboardIcon imageIcon={"../../src/assets/tourism_icon.png"} iconLabel={"Tourism"}/>*/}
-        {/*<DashboardIcon imageIcon={"../../src/assets/money_icon.png"} iconLabel={"Money"}/>*/}
-        {/*<DashboardIcon imageIcon={"../../src/assets/wifi_icon.png"} iconLabel={"Wifi"}/>*/}
-        {/*<DashboardIcon imageIcon={"../../src/assets/library_icon.png"} iconLabel={"Library"}/>*/}
-        {/*<DashboardIcon imageIcon={"../../src/assets/Emergency_icon.png"} iconLabel={"Emergency"}/>*/}
+        {/*{Object.values(layerProvider.getLayers()).map(layer=>{*/}
+        {/*  return(*/}
+        {/*      <DashboardIcon key={layer.tagName} handleClick={layerProvider.handleClick} imageIcon={layer.imageUrl} iconLabel={language[layer.tagName]}/>*/}
+        {/*  )*/}
+        {/*})}*/}
+        <DashboardIcon imageIcon={"../../src/assets/food_icon.png"} iconLabel={language["Food"]} handleClick={layerProvider.handleClick}/>
+        <DashboardIcon imageIcon={"../../src/assets/bathroom_icon.png"} iconLabel={language["Bathroom"]} handleClick={layerProvider.handleClick}/>
+        <DashboardIcon imageIcon={"../../src/assets/trash_icon.png"} iconLabel={language["Trash"]} handleClick={layerProvider.handleClick}/>
+        <DashboardIcon imageIcon={"../../src/assets/tourism_icon.png"} iconLabel={language["Tourism"]} handleClick={layerProvider.handleClick}/>
+        <DashboardIcon imageIcon={"../../src/assets/money_icon.png"} iconLabel={language["Bank"]} handleClick={layerProvider.handleClick}/>
+        <DashboardIcon imageIcon={"../../src/assets/wifi_icon.png"} iconLabel={language["Wifi"]} handleClick={layerProvider.handleClick}/>
+        <DashboardIcon imageIcon={"../../src/assets/library_icon.png"} iconLabel={language["Library"]} handleClick={layerProvider.handleClick}/>
+        <DashboardIcon imageIcon={"../../src/assets/Emergency_icon.png"} iconLabel={language["Emergency"]} handleClick={layerProvider.handleClick}/>
       </div>
     </div>
   );

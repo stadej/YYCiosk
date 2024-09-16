d83a105ae26612b183a8b499f9506e649ff4875d
stadej <jamie.stade@ucalgary.ca>
2024-09-16 14:43:47 -0600

    A few aesthetic fixes

--- frontend/components/ui/DashboardIcon.jsx
+++ frontend/components/ui/DashboardIcon.jsx
@@ -3,13 +3,11 @@ import '../styles/Dashboard.css'
 
 function DashboardIcon(props) {
   return (
-    <div style={{ backgroundColor: '#C8102E', height: '12rem', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', height: '4rem', width: '4rem', borderRadius: '1rem', backgroundColor: 'white', marginLeft: '4rem', marginRight: '4rem'}}>
-            <div>
-              <img  src={props?.imageIcon} onClick={props.handleClick} id={props?.iconLabel} alt={props?.iconLabel + ' Icon'} style={{height: '8rem', width: '8rem'}} />
-              <span style={{fontWeight: '600', color: 'white', fontFamily: 'sans-serif', fontSize: '1.25rem'}}>{props?.iconLabel}</span>
-            </div>
+    <div style={{ backgroundColor: '#C8102E', height: '12rem', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', height: '8rem', width: '8rem', borderRadius: '1rem', backgroundColor: 'white', marginLeft: '2rem', marginRight: '2rem'}}>
+          <img  src={props?.imageIcon} onClick={props.handleClick} id={props?.iconLabel} alt={props?.iconLabel + ' Icon'} style={{height: '8rem', width: '8rem'}} />
         </div>
+        <span style={{fontWeight: '600', color: 'white', fontFamily: 'sans-serif', fontSize: '1.25rem'}}>{props?.iconLabel}</span>
     </div>
   );
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,16 +44,18 @@ function App() {
     <LanguageProvider>
       <LocationProvider>
           <LayersProvider>
-            <header className="absolute w-screen h-screen flex flex-col items-center justify-start gap-[31px] text-left text-29xl text-crimson font-open-sans mq675:gap-[31px]">
-              <Navbar/>
-                <div className="w-full h-full bg-white overflow-hidden flex flex-col items-center justify-end py-[10px] px-[10px] box-border gap-[20px]">
-                  
-                  <Map />
-                    <div className="w-full flex flex-row items-start justify-self-end">
-                      <Iconbar />
-                    </div>
+            <div className='root'>
+              <header className="relative w-full h-full flex flex-col items-center justify-start gap-[31px] text-left text-29xl text-crimson font-open-sans mq675:gap-[31px]">
+                <Navbar/>
+                  <div className="w-full h-full bg-white overflow-hidden flex flex-col items-center justify-end py-[10px] px-[10px] box-border gap-[20px]">
+                    
+                    <Map />
+                      <div className="w-full flex flex-row items-start justify-self-end">
+                        <Iconbar />
+                      </div>
                 </div>
               </header>
+            </div>
           </LayersProvider>
       </LocationProvider>
     </LanguageProvider>

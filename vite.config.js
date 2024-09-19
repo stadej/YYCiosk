3947b38cd81da4039aff5ea3927788881fb0e26c
stadej <jamie.stade@ucalgary.ca>
2024-09-19 15:36:57 -0600

    changed base again

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: './YYCiosk',
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
